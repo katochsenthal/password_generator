0daedd38caa06c0c040925514cec4005a07895af
katochsenthal <katoch.senthal@gmail.com>
2022-03-12 14:28:27 -0500

    added function to generated password

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,91 @@
 // Assignment code here
+var passwordCharSelection = {
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  specialChar: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+};
 
+var generatePassword = function () {
+  // prompt asking number of characters
+  var password = "";
+
+  var charLength = window.prompt(
+    "How many characters would you like your password to be? Choose between 8 and 128"
+  );
+
+  // if user choose character less than 7 or more than 128.
+  while (charLength <= 7 || charLength >= 128) {
+    window.alert("Password must be between  8 and 128 characters. Try again!");
+    var charLength = window.prompt(
+      "How many characters would you like your password to be? Choose between 8 and 128"
+    );
+  }
+
+  // prompt for asking if the user wants numbers in the password
+  var confirmNumbers = window.confirm("Will your password have numbers in it?");
+
+  if (confirmNumbers) {
+    password += passwordCharSelection.numbers;
+  }
+
+  // prompt for asking if the user wants lowerCase letters in the password
+  var confirmLowerCase = window.confirm(
+    "Will your password have lowercase letters in it?"
+  );
+
+  if (confirmLowerCase) {
+    password += passwordCharSelection.lowerCase;
+  }
+
+  // prompt for asking if the user wants  uppercase letters in the password
+  var confirmUpperCase = window.confirm(
+    "Will your password have uppercase letters in it?"
+  );
+
+  if (confirmLowerCase) {
+    password += passwordCharSelection.upperCase;
+  }
+
+  // prompt for asking if the user wants special Characters in the password?
+  var specialCharacters = window.confirm(
+    "Will your password have any special Characters in it?"
+  );
+
+  if (specialCharacters) {
+    password += passwordCharSelection.specialChar;
+  }
+
+  // if user skips numbers, uppercase, lowercase and special characters
+  while (
+    !confirmNumbers &&
+    !confirmLowerCase &&
+    !confirmUpperCase &&
+    !specialCharacters
+  ) {
+    window.alert(
+      "You must select at least one character type required in the password!"
+    );
+
+    var confirmNumbers = window.confirm(
+      "Will your password have numbers in it?"
+    );
+    var confirmLowerCase = window.confirm(
+      "Will your password have lowercase letters in it?"
+    );
+    var confirmUpperCase = window.confirm(
+      "Will your password have uppercase letters in it?"
+    );
+    var specialCharacters = window.confirm(
+      "Will your password have any special Characters in it?"
+    );
+  }
+  var randomPassword = [];
+  for (var i = 0; i < charLength; i++) {
+    randomPassword += password[Math.floor(Math.random() * password.length)];
+  }
+  return randomPassword;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +96,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
